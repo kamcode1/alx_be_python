ca402056b39ff867ac8bacee935dcad96a414c17
Kam Ephrem <kamnoah88@gmail.com>
2024-06-23 02:14:46 +0300

    basic_operations.py Edited

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,9 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number2 - number1
+mult = number1 * number2
 
+print("Addition of", number1, "and", number2, "is",  sum )
+print("Subtraction of", number1, "and", number2, "is",  sub )
+print("Multiplication of", number1, "and", number2, "is",  mult )
