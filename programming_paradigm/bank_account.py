de52c8572b5f27eefe8368ff2c02fc5b11cd00e1
Kam Ephrem <kamnoah88@gmail.com>
2024-07-14 01:03:11 +0100

    edited programming_paradigm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,14 @@
 class BankAccount:
-    def __init__(self, account_balance = 0.00):
-        self.account_balance = account_balance
+    def __init__(self, account_balance = 0.0):
+        self.account_balance = float(account_balance)
    
     def deposit(self, amount):
-        self.account_balance +=  amount
+        self.account_balance += float(amount)
         return self.account_balance
     
     def withdraw(self, amount):
-        if self.account_balance >= amount:
-            self.account_balance -= amount
+        if self.account_balance >= float(amount):
+            self.account_balance -= float(amount)
             return True
         else:
             return False
