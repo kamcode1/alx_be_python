2c8ed5b00b400aabcd49e6607cfdc45f745c32c9
Kam Ephrem <kamnoah88@gmail.com>
2024-07-17 00:11:45 +0100

    Added main.py, phone.py and item.py in OOP_practice directory

--- oop_practice/main.py
+++ oop_practice/main.py
@@ -1,34 +1,33 @@
-class Item:
-    pay_rate = 0.8 # The pay rate after 20% discount
-    all = []
-    def __init__(self, name: str, price, quantity = 0):
-        # Run validations to the received arguments
-        assert price >= 0, f"price {price} is not greater than or equal to zero!"     
-        assert quantity >= 0, f"quantity {quantity} is not greater than or equal to zero!"
+from item import Item
+from phone import Phone
 
 
-        # Assign to self object
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-        # Actions to execute
-        Item.all.append(self)
-
-    def calculate_total_price(self):
-        return self.price * self.quantity
-    
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
-
-    
-item1 = Item("phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("keyboard", 75, 5)
+Item.instantiate_from_csv()
 
 print(Item.all)
+        # Actions to execute
+        # Phone.all.append(self)
 
+# phone1 = Phone("jscPhonev10", 500, 5, 1)
+
+# print(Item.all)
+
+# print(Phone.all)
+
+
+
+
+
+
+
+
+
+
+# print(Item.is_integer(7.5))
+# Item.instantiate_from_csv()
+# print(Item.all)
+
+# for instance in Item.all:
+#     print(instance.name)
 # print(item2.price)
 # item2.has_numpad = False
